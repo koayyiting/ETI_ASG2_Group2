8059da9bb76a419d66278c5be83431d22e1a6c85
koayyiting <s10221765@connect.np.edu.sg>
2024-01-31 22:48:38 +0800

    changed some functions

--- Book/book.go
+++ Book/book.go
@@ -5,19 +5,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 type Booking struct {
-	StudentName  string `json:"Student Name"`
-	StudentEmail string `json:"Student Email"`
-	LessonID     string `json:"Lesson ID"` //link to schedule details
-	LessonName   string `json:"LessonName"`
-	Location     string `json:"Location"`
+	BookingID  string `json:"booking_id"`
+	StudentID  string `json:"student_id"`
+	ScheduleID string `json:"schedule_id"`
 }
 
 var (
@@ -27,20 +25,17 @@ var (
 
 func main() {
 	router := mux.NewRouter()
-	router.Use(corsMiddleware)
 	router.HandleFunc("/api/v1/book", newBook).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/v1/book/{bookingID}", existBooking).Methods("GET", "DELETE", "POST", "OPTIONS")
+	router.HandleFunc("/api/v1/getBookings", getBooking).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/v1/oneBooking/{bookingID}", oneBooking).Methods("GET", "DELETE", "POST", "OPTIONS")
 	fmt.Println("Listening at port 1765")
-	log.Fatal(http.ListenAndServe(":1765", router))
-}
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization")
-		next.ServeHTTP(w, r)
-	})
+	http.ListenAndServe(":1765",
+		handlers.CORS(
+			handlers.AllowedOrigins([]string{"*"}),
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}),
+			handlers.AllowedHeaders([]string{"Origin", "X-Api-Key", "X-Requested-With", "Content-Type", "Accept", "Authorization"}),
+			handlers.AllowCredentials(),
+		)(router))
 }
 
 func openDB() {
@@ -54,7 +49,7 @@ func openDB() {
 func newBook(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		retrieveBookings("") // need use session to get the email of student
+		retrieveBookings() // need use session to get the email of student
 	case http.MethodPost: //create booking
 		if body, err := io.ReadAll(r.Body); err == nil {
 			var newBooking Booking
@@ -83,45 +78,56 @@ func createBooking(b Booking) error {
 	fmt.Println("In createBooking function")
 	openDB()
 	defer db.Close()
-	_, err := db.Exec("insert into Booking (StudentName, StudentEmail, LessonID) values(?,?,?)", b.StudentName, b.StudentEmail, b.LessonID)
+	_, err := db.Exec("INSERT into Booking (StudentID, ScheduleID) values(?,?)", b.StudentID, b.ScheduleID)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-// retrieve all booking done by student
-// mb should do lesson name
-func retrieveBookings(email string) error {
-	fmt.Println("In retrieveBookings function")
-	openDB()
-	defer db.Close()
-	_, err := db.Exec("select LessonID from Booking where StudentEmail = ?", email)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func existBooking(w http.ResponseWriter, r *http.Request) {
-	bookingID := mux.Vars(r)["bookingID"]
-
+func getBooking(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case http.MethodPut:
-		if body, err := io.ReadAll(r.Body); err == nil {
-			var updatedBooking Booking
-			if err := json.Unmarshal(body, &updatedBooking); err == nil {
-				json.NewDecoder(r.Body).Decode(&updatedBooking)
-				if err := updateBooking(updatedBooking); err == nil {
-					w.WriteHeader(http.StatusAccepted) //202
-				} else {
-					w.WriteHeader(http.StatusConflict)
-				}
-
+	case http.MethodGet:
+		if bookings, err := retrieveBookings(); err == nil {
+			w.WriteHeader(http.StatusAccepted) //202
+			if bookingsJSON, err := json.Marshal(bookings); err == nil {
+				w.Write(bookingsJSON)
 			} else {
 				fmt.Println(err)
 			}
+		} else {
+			w.WriteHeader(http.StatusConflict)
 		}
+	}
+}
+
+// retrieve all booking done by student
+func retrieveBookings() ([]Booking, error) {
+	fmt.Println("In retrieveBookings function")
+
+	openDB()
+	rows, err := db.Query("SELECT * FROM Booking")
+	// rows, err := db.Query("SELECT * FROM Schedule WHERE TutorID = ?", tid)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	var bookings []Booking
+
+	for rows.Next() {
+		var booking Booking
+		if err := rows.Scan(&booking.BookingID, &booking.StudentID, &booking.ScheduleID); err != nil {
+			return nil, err
+		}
+		bookings = append(bookings, booking)
+	}
+	return bookings, nil
+}
+
+func oneBooking(w http.ResponseWriter, r *http.Request) {
+	bookingID := mux.Vars(r)["bookingID"]
+
+	switch r.Method {
 	case http.MethodDelete:
 		if status := deleteBooking(bookingID); status {
 			w.WriteHeader(http.StatusAccepted) //202
@@ -131,52 +137,15 @@ func existBooking(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// update Booking details
-// theres nothing much to update for now..
-func updateBooking(updatedBooking Booking) error {
-	fmt.Println("In updateBooking func")
-	openDB()
-	defer db.Close() //will run at the end of the block of the code
-	_, err := db.Exec("update Booking set StudentName=?, StudentEmail=?, where ID=?;", updatedBooking.StudentEmail, updatedBooking.StudentEmail)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 // delete booking
 func deleteBooking(id string) bool {
 	fmt.Println("in deleteBooking function")
 	openDB()
 	defer db.Close() //will run at the end of the block of the code
-	_, err := db.Exec("delete from Booking where ID = ?", id)
+	_, err := db.Exec("DELETE FROM Booking WHERE BookingID = ?", id)
 	if err != nil {
 		fmt.Println(err)
 		return false
 	}
 	return true
 }
-
-// for curl on cmd
-
-// var testing map[string]Booking = map[string]Booking{
-// 	"Test_ID": {StudentName: "koay yi ting", StudentEmail: "koayyiting@gmail.com", LessonID: "test_lesson_id"},
-// }
-
-// func book(w http.ResponseWriter, r *http.Request) {
-// 	bookingID := mux.Vars(r)["bookingID"]
-// 	switch r.Method {
-// 	case http.MethodGet:
-// 		chosenBooking, _ := testing[bookingID]
-// 		json.NewEncoder(w).Encode(chosenBooking)
-// 	case http.MethodPost: //create booking
-// 		var newBooking Booking
-// 		err := json.NewDecoder(r.Body).Decode(&newBooking)
-// 		if err != nil {
-// 			fmt.Fprintf(w, "Invalid json format\n")
-// 			return
-// 		}
-// 		testing[bookingID] = newBooking
-// 		w.WriteHeader(http.StatusAccepted)
-// 	}
-// }
