c0b3725122a476841e86869bc88ff35adb1b5af5
TanKwanWei <84904561+suicopath@users.noreply.github.com>
2024-02-03 20:01:36 +0800

    Updated student files and HTML/JS integration
    Added HTML site for Login, JS and CSS files
    Updated structure, added handlers for Student
    Added student database script

---
+++ student.js
@@ -0,0 +1,71 @@
+const login_btn = document.querySelector("#login-btn");
+const register_btn = document.querySelector("#register-btn");
+const container = document.querySelector(".container");
+
+register_btn.addEventListener("click", () => {
+  container.classList.add("register-mode");
+});
+
+login_btn.addEventListener("click", () => {
+  container.classList.remove("register-mode");
+});
+
+//student features
+function studentRegister(){
+    var request = new XMLHttpRequest();
+    const form = document.getElementById('studentRegisterForm');
+
+    const curl = 'http://localhost:2479/api/v1/student';
+
+    const studentEmail = form.elements['student_register_email'].value;
+    const studentPassword = form.elements['student_register_password'].value;
+    const studentFirstName = form.elements['student_register_firstname'].value;
+    const studentLastName = form.elements['student_register_lastname'].value;
+
+    console.log(studentEmail);
+    console.log(studentPassword);
+
+    request.open("POST", curl);
+    request.send(JSON.stringify({
+        "studentEmail": studentEmail,
+        "studentPassword": studentPassword, 
+        "studentFirstName": studentFirstName,
+        "studentLastName": studentLastName
+        
+    }));
+    form.reset();
+    alert("Tutor account registered successfuly.");
+    return false
+}
+
+function studentLogin(){
+    var request = new XMLHttpRequest();
+    const form = document.getElementById('studentLoginForm');
+    const studentEmail = form.elements['student_login_email'].value;
+    const studentPassword = form.elements['student_login_password'].value;
+    console.log(studentEmail);
+    console.log(studentPassword);
+
+    const curl = 'http://localhost:2479/api/v1/student?studentEmail=' + encodeURIComponent(studentEmail) + '&studentPassword=' + encodeURIComponent(studentPassword);
+    console.log(curl);
+
+    request.open("GET", curl);
+    request.onreadystatechange = function() {
+      if (request.readyState === 4) {
+        if (request.status === 200) {
+          // Successful login, redirect to main page
+          location.href = "student_main.html";
+        } else if (request.status === 401) {
+          // Login failed, handle error
+          form.reset();
+          document.getElementById('error-message').innerHTML = 'Incorrect Email or Password.';
+        } else {
+          // Handle other status codes or network errors
+          document.getElementById('error-message').innerHTML = 'Error encountered. Please try again later.';
+        }
+      }
+    };
+    request.send();
+    return false
+}
+
