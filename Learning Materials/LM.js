f702e58cfa15803a7b39f50133ce4e21e37227e3
LacsadoShaniahSantiago <s10204088@connect.np.edu.sg>
2024-02-06 22:18:27 +0800

    feat: Learning Material Website

--- Learning Materials/LM.js
+++ Learning Materials/LM.js
@@ -1,52 +1,58 @@
-var content = document.getElementById("lmContent");
+var content = document.querySelector("ul.box-info#lmContent");
 
 function getAllLM() {
 
     var GetRequest = new XMLHttpRequest()
-    GetRequest.open("GET", "http://localhost:4088//lessonmaterial/all")
+    GetRequest.open("GET", "http://localhost:4088/lessonmaterial/all")
 
+    console.log(getId())
     GetRequest.onload = function () {
         var data = JSON.parse(this.response)
         var lmList = Object.keys(data.Materials)
 
-        content.innerHTML = ""
         
+        console.log(lmList.length)
+        content.innerHTML = "";
+
         lmList.forEach((lmId, index) => {
 
             var lm = Object.keys(data.Materials[lmId])
-            console.log(lm)
 
             //HTML
-            var card = document.createElement('div')
-            card.className = 'card'
-            card.style = 'width: 18rem'
+            var lmItem = document.createElement("li");
+            
+            var lmIcon = document.createElement("i");
+            lmIcon.className = "bx bxs-chevron-right";
 
-            var cardBody = document.createElement('div')
-            cardBody.className = 'card-body'
+            var lmBody = document.createElement("span");
+            lmBody.className = "text";
 
-            var cardTitle = document.createElement('h5')
-            cardTitle.className = 'card-title'
-
-            var cardSub = document.createElement('h6')
-            cardSub.className = 'card-subtitle mb-2 text-muted'
-
-            var cardText = document.createElement('p')
-            cardText.className = 'card-text'
+            var lmHeader = document.createElement("h3");
+            // var lmSummary = document.createElement("p");
+            var lmCreated = document.createElement("footer");
+            lmCreated.className = "blockquote-footer";
 
             lm.forEach((material, index2) => {
-                console.log(material)
-                cardTitle.innerHTML +=  ((material == "Topic") ? data.Materials[lmId][material] : '')
+                lmHeader.innerHTML +=  ((material == "Topic") ? data.Materials[lmId][material] : '')
 
-                cardSub.innerHTML += ((material == "Created on") ? formatDate(data.Materials[lmId][material]) : '')
-                cardText.innerHTML +=((material == "Summary") ? data.Materials[lmId][material] : '')
+                lmCreated.innerHTML += ((material == "Created on") ? formatDate(data.Materials[lmId][material]) : '')
+                // lmSummary.innerHTML +=((material == "Summary") ? data.Materials[lmId][material] : '')
             })
 
-            cardBody.appendChild(cardTitle)
-            cardBody.appendChild(cardSub)
-            cardBody.appendChild(cardText)
-        
-            card.appendChild(cardBody)
-            content.appendChild(card)
+            console.log(lmItem)
+
+            lmBody.appendChild(lmHeader);
+            // lmBody.appendChild(lmSummary);
+
+            var brline = document.createElement("br")
+            lmBody.appendChild(brline)
+            
+            lmBody.appendChild(lmCreated);
+            
+            lmItem.appendChild(lmIcon);
+            lmItem.appendChild(lmBody);
+
+            content.appendChild(lmItem);
 
         });
     }
@@ -60,8 +66,6 @@ function formatDate(datetimeStr) {
         day: '2-digit',
         month: 'long',
         year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
         hour12: true
     }).replace(/ at /g, ' ');
 
@@ -69,11 +73,32 @@ function formatDate(datetimeStr) {
 
 }
 
+function getId() {
+
+    fetch("http://localhost:4088/lessonmaterial/all")
+        .then(response => response.json())
+        .then(data => {
+            var lmList = Object.keys(data.Materials)
+            console.log(lmList.length)
+        })
+
+
+
+
+}
+// function getTutorId() {
+
+//     var GetRequest = new XMLHttpRequest()
+//     const newID
+// }
+
 function addLM() {
 
     var addRequest = new XMLHttpRequest()
     const newID = document.getElementById("id").value
     addRequest.open("POST", "http://localhost:5000//lessonmaterial/" + newID)
+
+    console.log(addRequest)
 
     const newLMJSON = {
         "TutorID" : document.getElementById("tutorId").value,
@@ -85,12 +110,14 @@ function addLM() {
     addRequest.onload = function () {
         if(addRequest.status == 202) {
             alert('Learning Material is successfully created')
-            windows.location.href="LM.html"
+            windows.location.href="../Learning Materials/LM.html"
         } else if (addRequest.status == 409) {
             alert('Learning Material is not created')
-            windows.location.href="LM.html"
+            windows.location.href="../Learning Materials/LM.html"
         }
     }
 
     addRequest.send(JSON.stringify(newLMJSON))
+
+
 }
--- Learning Materials/LessonMaterial.sql
+++ Learning Materials/LessonMaterial.sql
@@ -12,9 +12,14 @@ CREATE TABLE LessonMaterials (
 LMID VARCHAR(5) NOT NULL PRIMARY KEY,
 TutorID	INT NOT NULL,
 Topic VARCHAR(100) NULL,
-Summary VARCHAR(9000) NULL,
+Summary VARCHAR(400) NULL,
 Created DATETIME NULL 
 );
 
 INSERT INTO LessonMaterials (LMID, TutorID, Topic, Summary, Created)
-VALUES("1","1", "Basic Go Programming Language", "Go, often referred to as Golang, is a statically-typed programming language developed by Google. It was created to address issues like slow compilation times and complex dependencies in other languages.", "2023-01-02 15:04:05")
+VALUES("1","1", "Basic Go Programming Language", "Go, often referred to as Golang, is a statically-typed programming language developed by Google. It was created to address issues like slow compilation times and complex dependencies in other languages.", "2023-01-02 15:04:05"),
+	("2","1", "Basic Python Programming Language", "Python's easy-to-read syntax, resembling plain English, makes it perfect for beginners and experienced programmers alike. It also boasts concise code, often requiring fewer lines compared to other languages, saving you time and effort.", "2023-11-21 15:32:47"),
+	("3","1", "Basic C# Programming Language", "C# is a modern language developed by Microsoft for building various applications, known for its strong typing, automatic garbage collection, and object-oriented programming support. It's often used with the .NET framework and is highly integrated with the Windows operating system.", "2024-06-08 09:15:02"),
+    ("4","2","Basic C++ Programming Language","The C programming language, created by Dennis Ritchie in 1972, is a powerful and efficient language used for system programming, embedded systems, and application development. It provides low-level access to memory and hardware, making it suitable for developing operating systems and other performance-critical software.","2024-06-05 09:15:02"),
+    ("5","2","Basic C Programming Language","The C programming language, created by Dennis Ritchie in 1972, is a powerful and widely used language for system programming, embedded systems, and application development. Known for its efficiency and flexibility, C has influenced many other programming languages and remains an important tool for software development.","2024-04-27 16:44:50"),
+    ("6","2","DevOps Fundamentals","DevOps is a software development approach that combines development (Dev) and operations (Ops) to improve collaboration and automation. It emphasizes communication, integration, automation, and measurement to streamline the software delivery process and ensure high-quality, rapid deployment of applications.","2023-11-21 15:32:47")
