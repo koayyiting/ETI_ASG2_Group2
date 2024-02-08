8117e256d70992ab66bb93fa53b06fae500a6056
koayyiting <s10221765@connect.np.edu.sg>
2024-02-09 05:24:16 +0800

    booking sql

--- Book/booking_db_setup.sql
+++ Book/booking_db_setup.sql
@@ -1,20 +1,20 @@
 -- CREATE USER 'booking_system'@'localhost' IDENTIFIED BY 'ETI_Group2_Booking';
 -- GRANT ALL ON *.* TO 'booking_system'@'localhost';
 USE ETI_Booking;
-SELECT User, Host FROM mysql.user WHERE User = 'booking_system' AND Host = 'localhost';
+-- SELECT User, Host FROM mysql.user WHERE User = 'booking_system' AND Host = 'localhost';
 -- CREATE database ETI_Booking;
 
-drop table Booking
--- CREATE TABLE Booking (
--- BookingID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
--- StudentID INT NOT NULL, 
--- ScheduleID INT NOT NULL,
--- LessonID varchar(100)
--- );
 
+CREATE TABLE Booking (
+    BookingID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    StudentID INT NOT NULL, 
+    ScheduleID INT NOT NULL
+);
+
+-- Editing the Tables --
 -- insert into Booking (StudentName, StudentEmail, LessonID) values("Zi Yi","test","1");
 -- DELETE FROM Booking WHERE ID = 1;
-
+-- drop table Booking
 -- ALTER TABLE Booking AUTO_INCREMENT = 1; 
 
 select * from Booking;
